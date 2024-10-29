98f58b4d5a6ec8b061d2b1e6de9ca399976fd50f
Edu Lopez <edulopez.dev@gmail.com>
2024-10-28 22:54:28 -0300

    Index.pug
    Vista principal, se agrega lógica para mostrar turnos ocupados y libres

--- public/js/frontMain.js
+++ public/js/frontMain.js
@@ -2,26 +2,39 @@ document.addEventListener('DOMContentLoaded', function () {
     const turnosList = document.getElementById("resultadosContainer");
 
     turnosList.addEventListener("click", function (event) {
-        const turnoSeleccionado = event.target.closest("li");
-        if (turnoSeleccionado) {
-            const numero_turno = turnoSeleccionado.getAttribute("data-id");
-            const nombrePaciente = turnoSeleccionado.getAttribute("data-nombre");
+        const cardSeleccionada = event.target.closest(".card");
+        if (cardSeleccionada) {
+            const horaTurno = cardSeleccionada.querySelector('.card-title').innerText; // Obtener la hora del turno
+            
+            // Cambia aquí para usar window.turnos en lugar de solo turnos.
+            const turnoInfo = window.turnos[horaTurno]; 
 
-            // Usar SweetAlert2 para mostrar el modal de confirmación
-            Swal.fire({
-                title: 'Confirmar Consulta',
-                text: `¿Está seguro de que desea iniciar la consulta para el paciente: ${nombrePaciente}?`,
-                icon: 'question',
-                showCancelButton: true,
-                confirmButtonColor: '#28a745',
-                cancelButtonColor: '#dc3545',
-                confirmButtonText: 'Sí, iniciar consulta',
-                cancelButtonText: 'No, cancelar',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = `/getConsulta?numero_turno=${numero_turno}`;
-                }
-            });
+            if (turnoInfo) { 
+                const nombrePaciente = `${turnoInfo.nombre} ${turnoInfo.apellido}`;
+                const numero_turno = horaTurno;
+
+                Swal.fire({
+                    title: 'Confirmar Consulta',
+                    text:`¿Está seguro de que desea iniciar la consulta para el paciente:${nombrePaciente}?`,
+                    icon:'question',
+                    showCancelButton:true,
+                    confirmButtonColor:'#28a745',
+                    cancelButtonColor:'#dc3545',
+                    confirmButtonText:'Sí, iniciar consulta',
+                    cancelButtonText:'No, cancelar',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = `/getConsulta?numero_turno=${numero_turno}`;
+                    }
+                });
+            } else {
+                Swal.fire({
+                    title:'Turno Libre',
+                    text:'Este turno está libre. No se puede iniciar una consulta.',
+                    icon:'info',
+                    confirmButtonText:'Aceptar'
+                });
+            }
         }
     });
 });
