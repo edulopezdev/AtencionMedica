710da2da86c319e1e8dd30753b371d8fe99636ae
Edu Lopez <edulopez.dev@gmail.com>
2024-10-30 00:58:23 -0300

    Manejo de vista index
    Se agregan condiciones de estado, modal,

--- public/js/frontMain.js
+++ public/js/frontMain.js
@@ -6,34 +6,49 @@ document.addEventListener('DOMContentLoaded', function () {
         if (cardSeleccionada) {
             const horaTurno = cardSeleccionada.querySelector('.card-title').innerText; // Obtener la hora del turno
             
-            // Cambia aquí para usar window.turnos en lugar de solo turnos.
             const turnoInfo = window.turnos[horaTurno]; 
 
             if (turnoInfo) { 
-                const nombrePaciente = `${turnoInfo.nombre} ${turnoInfo.apellido}`;
-                const numero_turno = horaTurno;
-                const numero = turnoInfo.numero;
+                if (turnoInfo.estado.toLowerCase() === 'atendido') {
+                    Swal.fire({
+                        title: 'Turno Atendido',
+                        text: 'Este turno ya fue atendido.',
+                        icon: 'info',
+                        confirmButtonText: 'Aceptar'
+                    });
+                } else if (turnoInfo.estado.toLowerCase() === 'cancelado') {
+                    Swal.fire({
+                        title: 'Turno Cancelado',
+                        text: 'Este turno ha sido cancelado y no se puede iniciar una consulta.',
+                        icon: 'warning',
+                        confirmButtonText: 'Aceptar'
+                    });
+                } else {
+                    const nombrePaciente = `${turnoInfo.nombre} ${turnoInfo.apellido}`;
+                    const numero_turno = horaTurno;
+                    const numero = turnoInfo.numero;
 
-                Swal.fire({
-                    title: 'Confirmar Consulta',
-                    text:`¿Está seguro de que desea iniciar la consulta para el paciente:${nombrePaciente}?`,
-                    icon:'question',
-                    showCancelButton:true,
-                    confirmButtonColor:'#28a745',
-                    cancelButtonColor:'#dc3545',
-                    confirmButtonText:'Sí, iniciar consulta',
-                    cancelButtonText:'No, cancelar',
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = `/getConsulta?hora=${numero_turno}&numero_turno=${numero}`;
-                    }
-                });
+                    Swal.fire({
+                        title: 'Confirmar Consulta',
+                        text: `¿Está seguro de que desea iniciar la consulta para el paciente: ${nombrePaciente}?`,
+                        icon: 'question',
+                        showCancelButton: true,
+                        confirmButtonColor: '#28a745',
+                        cancelButtonColor: '#dc3545',
+                        confirmButtonText: 'Sí, iniciar consulta',
+                        cancelButtonText: 'No, cancelar',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = `/getConsulta?hora=${numero_turno}&numero_turno=${numero}`;
+                        }
+                    });
+                }
             } else {
                 Swal.fire({
-                    title:'Turno Libre',
-                    text:'Este turno está libre. No se puede iniciar una consulta.',
-                    icon:'info',
-                    confirmButtonText:'Aceptar'
+                    title: 'Turno Libre',
+                    text: 'Este turno está libre. No se puede iniciar una consulta.',
+                    icon: 'info',
+                    confirmButtonText: 'Aceptar'
                 });
             }
         }
