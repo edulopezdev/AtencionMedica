190b3c49f50285808ac7d5d955607019d164f882
Edu Lopez <edulopez.dev@gmail.com>
2024-11-12 14:08:11 -0300

    Estilos botones.

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql');
 
 //Direccion DB Franco
-const conexion = mysql.createConnection({
-   host: 'localhost',
-   port: 3307,
-   user: 'root',
-   password: '',
-   database: 'am_tp'
-//    database: 'atencion_medica'
-}); 
+// const conexion = mysql.createConnection({
+//    host: 'localhost',
+//    port: 3307,
+//    user: 'root',
+//    password: '',
+//    database: 'am_tp'
+// //    database: 'atencion_medica'
+// }); 
 
 //Direccion DB Edu
-//  const conexion = mysql.createConnection({
-//      host: 'localhost',
-//      port: 3306,
-//      user: 'root',
-//      password: '',
-//      database: 'am_tp' 
-//  });
+ const conexion = mysql.createConnection({
+     host: 'localhost',
+     port: 3306,
+     user: 'root',
+     password: '',
+     database: 'am_tp' 
+ });
 
 //Acá verificamos la conexión
 conexion.connect((error) => {
